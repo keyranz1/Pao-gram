c90f71ee4f3bd658b29ad277113177e898276458
Kiran Yadubanshi <bansi@Kirans-MacBook-Pro.local>
2018-09-16 02:42:54 -0500

    Added google maps to app and configured it to be marked on screen

--- src/models/location.ts
+++ src/models/location.ts
@@ -1,6 +1,6 @@
 
 export class Location{
-  constructor(public lat: number, public long: number){
+  constructor(public lat: number, public lng: number){
 
   }
 }
--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SetLocationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavParams } from 'ionic-angular';
+import {Location} from "../../models/location";
 
 @IonicPage()
 @Component({
@@ -14,12 +8,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'set-location.html',
 })
 export class SetLocationPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  location: Location;
+   marker: Location;
+  constructor(private navParams: NavParams){
+    this.location = this.navParams.get('location');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SetLocationPage');
+  onSetMarker(event: any){
+    console.log(event);
+    this.marker = new Location(event.coords.lat, event.coords.lng);
   }
-
 }
